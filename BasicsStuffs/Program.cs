c3316ef04e0e7e0cce515c43274736c5941d6ef8
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-08-06 22:00:25 +0600

    Initial commit

---
+++ BasicsStuffs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicsStuffs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] freq = new int[101];
+            // Csharp initializes array with 0
+            Dictionary<int, int> mapping = new Dictionary<int, int>(10);
+
+            mapping.Add(0, 5);
+            mapping.Add(1, 3);
+            mapping.Add(2, 9);
+
+            foreach(KeyValuePair<int, int> keyValue in mapping)
+            {
+                Console.WriteLine(keyValue);
+                Console.WriteLine(keyValue.Key);
+            }
+
+            
+        }
+    }
+}
