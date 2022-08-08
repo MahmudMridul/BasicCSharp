b4c7e1cde1294460c8778504a61eede7818c4e4f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-08-08 20:49:52 +0600

    ListTest

---
+++ BasicsStuffs/List/ListTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicsStuffs.List
+{
+    public class ListTest
+    {
+        public ListTest()
+        {
+            IList<int> list = new List<int>();
+            list.Add(5);
+            list.Add(2);
+            list.Add(1);
+            list.Add(7);
+
+            Console.WriteLine(list[1]);
+
+            list.RemoveAt(1);
+
+            Console.WriteLine(list[1]);
+
+        }
+    }
+}
--- BasicsStuffs/Program.cs
+++ BasicsStuffs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using BasicsStuffs.Array;
+using BasicsStuffs.List;
 
 namespace BasicsStuffs
 {
@@ -8,14 +9,43 @@ namespace BasicsStuffs
     {
         static void Main(string[] args)
         {
-            new JaggedArrayTest();
+
+            //new ListTest();
+
+            int winner = new Program().FindTheWinner(6, 5);
+            Console.WriteLine(winner);
 
 
-            
 
-            
 
-            
+        }
+
+        public int FindTheWinner(int n, int k)
+        {
+            IList<int> players = new List<int>(n);
+
+            for(int player = 1; player <= n; ++player)
+            {
+                players.Add(player);
+            }
+
+            int remainingPlayers = n;
+            int currentPlayer = 1;
+
+            while(remainingPlayers > 1)
+            {
+                int losingPlayer = (currentPlayer + (k - 1)) % n;
+                players.RemoveAt((losingPlayer - 1)%n);
+                currentPlayer = (losingPlayer) % n;
+                --remainingPlayers;
+            }
+
+            foreach(int player in players)
+            {
+                return player;
+            }
+
+            return -1;
         }
     }
 }
